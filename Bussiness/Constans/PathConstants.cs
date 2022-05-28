e7d96caae948855106f8fc5500d073529ca201d7
denizatuk <dnz.atk96@gmail.com>
2022-05-28 07:00:02 +0300

    12

---
+++ Bussiness/Constans/PathConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Constans
+{
+    public class PathConstants
+    {
+        public static string ImagesPath = "wwroot\\Uploads\\Images\\";
+    }
+}
